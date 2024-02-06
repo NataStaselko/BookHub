fa1af9d051ec743e3b1b91e62e5b88b26de2142c
Staselko <nat06024@gmail.com>
2024-02-06 20:03:04 +0300

    add author: crud, service, api

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.db.database import connect, disconnect
 from app.genres.api import router_genre
+from app.authors.api import router_author
 
 
 @asynccontextmanager
@@ -15,3 +16,4 @@ async def lifespan(fast: FastAPI):
 app = FastAPI()
 
 app.include_router(router_genre)
+app.include_router(router_author)
